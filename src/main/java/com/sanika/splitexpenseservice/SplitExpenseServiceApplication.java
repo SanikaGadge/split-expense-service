5bab61e5bb9fb6a6f19939620088d46e0dd04d47
Sanika Gadge <sanikagadge15@gmail.com>
2026-06-08 15:45:02 +0530

    Initialize Spring Boot project and add health check endpoint

---
+++ src/main/java/com/sanika/splitexpenseservice/SplitExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanika.splitexpenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanika/splitexpenseservice/SplitExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanika.splitexpenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitExpenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
